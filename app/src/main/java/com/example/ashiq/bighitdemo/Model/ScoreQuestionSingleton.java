daac9f91d4c3c45be42ce18d9f5d0178e5a1e8e5
Ashiq <AShiq.racer@gmail.com>
2019-01-03 00:49:55 +0600

    fragment to constraints 2

--- app/src/main/java/com/example/ashiq/bighitdemo/Model/ScoreQuestionSingleton.java
+++ app/src/main/java/com/example/ashiq/bighitdemo/Model/ScoreQuestionSingleton.java
@@ -1,8 +1,24 @@
 package com.example.ashiq.bighitdemo.Model;
 
+        import android.content.Context;
+
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.util.ArrayList;
+
 public class ScoreQuestionSingleton {
 
-    
 
+    private Context context;
+    public ScoreQuestionSingleton(Context context) {
 
+        this.context=context;
+
+    }
+
+    public ArrayList getQuestions()
+    {
+
+        return null;
+    }
 }
